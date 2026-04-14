7c7e3af75e4fadbbcac2dba2d47b7710b3f5dcdc
Darshan G <darshan.script@gmail.com>
2026-04-14 12:10:23 +0530

    add MongoDB connection with mongoose and environment configuration

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const dotenv = require("dotenv");
 const app = require("./app");
 
+const mongoose = require("mongoose");
 
-dotenv.config({path : "./.env"});
+dotenv.config({ path: "./.env" });
 
-const port = process.env.PORT || 5000
-const server = app.listen(port , ()=>{
-  console.log('API is running');
-})
+mongoose
+  .connect(process.env.CONN_STR)
+  .then((conn) => {
+    console.log("DB connection successful");
+
+    const port = process.env.PORT || 5000;
+    const server = app.listen(port, () => {
+      console.log("API is running");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
