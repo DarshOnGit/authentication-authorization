9f22a2534ed3059b4801acdc491ff32263c71258
Darshan G <darshan.script@gmail.com>
2026-04-17 20:55:01 +0530

    add email verification flow with JWT and nodemailer

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require("../models/userModel");
 const signUpSchema = require("./validateSchema");
+const sendEmail = require("../utils/email");
+const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res, next) => {
   try {
@@ -15,13 +17,32 @@ exports.signup = async (req, res, next) => {
       });
     }
 
-    const newUser = await user.create({
+    const newUser = await User.create({
       name,
       email,
       password,
     });
 
-    
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_ACCESS_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES,
+    });
+
+    const verifyUrl = `${req.protocol}://${req.get("host")}/api/auth/verify-email?token=${token}`;
+    const message = `Verify your Email . please use the below link to verify your email
+      \n\n ${verifyUrl}`;
+
+    try {
+      await sendEmail({
+        email: newUser.email,
+        subject: "Email verification!",
+        message
+      });
+    } catch (err) {
+      return res.status(500).json({
+        status: "fail",
+        message: "Email verification failed try again later",
+      });
+    }
 
     res.status(201).json({
       status: "success",
@@ -30,9 +51,54 @@ exports.signup = async (req, res, next) => {
       },
     });
   } catch (err) {
-    res.status(400).json({
+    res.status(500).json({
       status: "fail",
-      message: err
+      message: err,
     });
   }
 };
+
+
+exports.verifyEmail = async(req,res,next)=>{
+  const token = typeof req.query.token === "string" ? req.query.token : undefined ;
+
+  if(!token){
+    return res.status(400).json({
+      status : "fail",
+      message : 'verification token is missing'
+    })
+  }
+
+  try{
+    const decodedToken = jwt.verify(token , process.env.JWT_ACCESS_SECRET);
+
+    const user = await User.findById(decodedToken.id);
+
+    if(!user){
+      return res.status(401).json({
+        status:"fail",
+        message : "the user with the given token does not exist!"
+      })
+    }
+
+    if(user.isEmailVerified){
+      return res.status(400).json({
+        message:"Email is already verified"
+      })
+    }
+
+    user.isEmailVerified = true ;
+    await user.save();
+
+    return res.status(200).json({
+        message:"Email is now verified! You can login"
+      })
+  }
+  catch(err){
+    return res.status(500).json({
+      status:"fail",
+      message : "internal server error"
+    })
+  }
+
+}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authController = require("../controllers/authController")
 
 router.route("/signup").post(authController.signup);
+router.route("verify-email").get(authController.verifyEmail);
 
 module.exports = router;
